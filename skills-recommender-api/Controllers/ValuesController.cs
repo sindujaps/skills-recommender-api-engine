0c2a47f231b794b649d1f69e01656ce2e84f1648
Sinduja <sindujap@microsoft.com>
2023-07-12 15:51:07 -0500

    added hello get for testing

---
+++ skills-recommender-api/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace skills_recommender_api.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task <IActionResult> Get(){
+
+            return Ok("hello");
+        }
+    }
+}
